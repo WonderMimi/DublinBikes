3da1d5e55938c7c8f815b0ce410133bbb488ca54
LittleMy <littlemy68@gmail.com>
2019-08-17 15:45:28 +0200

    add functionnal slider with keypress

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,47 @@
-$(document).ready(function(){
-	$('.nextBtn').on('click', function(){
-	  var currentImg = $('.active');
-	  var nextImg = currentImg.next();
-  
-	  if(nextImg.length){
-		currentImg.removeClass('active').css('z-index', -10);
-		nextImg.addClass('active').css('z-index', 10);
-	  }
+$(document).ready(function () {
+	var currentImg;
+	var nextImg;
+	var prevImg;
+
+	$(".nextBtn").on("click", function () {
+		currentImg = $(".active");
+		nextImg = currentImg.next();
+
+		if (nextImg.length) {
+			currentImg.removeClass("active").css("z-index", -10);
+			nextImg.addClass("active").css("z-index", 10);
+		}
 	});
-  
-	$('.prevBtn').on('click', function(){
-	  var currentImg = $('.active');
-	  var prevImg = currentImg.prev();
-  
-	  if(prevImg.length){
-		currentImg.removeClass('active').css('z-index', -10);
-		prevImg.addClass('active').css('z-index', 10);
-	  }
+
+	$(".prevBtn").on("click", function () {
+		currentImg = $(".active");
+		prevImg = currentImg.prev();
+
+		if (prevImg.length) {
+			currentImg.removeClass("active").css("z-index", -10);
+			prevImg.addClass("active").css("z-index", 10);
+		}
 	});
-  });
+
+	document.addEventListener("keydown", function (e) {
+
+		if (e.keyCode === 37) {
+			currentImg = $(".active");
+			prevImg = currentImg.prev();
+	
+			if (prevImg.length) {
+				currentImg.removeClass("active").css("z-index", -10);
+				prevImg.addClass("active").css("z-index", 10);
+			}
+
+		} else if (e.keyCode === 39) {
+			currentImg = $(".active");
+			nextImg = currentImg.next();
+	
+			if (nextImg.length) {
+				currentImg.removeClass("active").css("z-index", -10);
+				nextImg.addClass("active").css("z-index", 10);
+			}
+		}
+	});
+});
